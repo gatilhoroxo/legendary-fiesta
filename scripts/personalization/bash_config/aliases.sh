b21f3da01269dfc32cbbe3527ff825aeff5bb08f
Drey <gatilhoroxo@gmail.com>
2025-11-30 02:51:58 +0000

    feat: new colors and functions

--- scripts/personalization/bash_config/aliases.sh
+++ scripts/personalization/bash_config/aliases.sh
@@ -10,6 +10,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
+
 # facilitar navegação
 shopt -s autocd
 shopt -s cdspell
@@ -36,4 +37,41 @@ alias update='sudo apt update -y && sudo apt upgrade -y'
 # outros
 
 
+#cores
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export NC='\033[0m'
 
+# Reset / Sem Cor
+export nc='\033[0m'       # No Color (Reseta tudo)
+
+# Cores Regulares (Texto Normal)
+export black='\033[0;30m'
+export red='\033[0;31m'
+export green='\033[0;32m'
+export yellow='\033[0;33m'
+export blue='\033[0;34m'
+export purple='\033[0;35m'
+export cyan='\033[0;36m'
+export white='\033[0;37m'
+
+# Cores em Negrito / Brilhantes (Mais usadas em terminais escuros)
+export b_black='\033[1;30m'  # Cinza escuro
+export b_red='\033[1;31m'
+export b_green='\033[1;32m'
+export b_yellow='\033[1;33m'
+export b_blue='\033[1;34m'
+export b_purple='\033[1;35m'
+export b_cyan='\033[1;36m'
+export b_white='\033[1;37m'
+
+# outras cores e estilos
+export g_black='\033[0;40m' # preto de fundo
+export bl_red='\033[1;91m'   # vermelho claro em negrito
+
+# Estilos Extras
+export italic='\033[3m'      # Faz o italico
+export underline='\033[4m'   # Sublinhado
+export blink='\033[5m'       # Piscar (nem todos terminais suportam)
+export reverse='\033[7m'     # Inverte fundo e texto
+export riscado='\033[9m'     # Risca o texto
--- scripts/personalization/bash_config/functions.sh
+++ scripts/personalization/bash_config/functions.sh
@@ -141,3 +141,25 @@ beginUser(){
   sudo apt autoremove -y
 }
 
+# Função para testar mensagens coloridas
+print_status() {
+    echo -e "${b_green}[SUCESSO]${nc} O comando foi executado."
+    echo -e "${b_red}[ERRO]${nc} Algo deu errado no processo."
+    echo -e "${b_yellow}[AVISO]${nc} Tenha cuidado com este comando."
+    echo -e "${b_cyan}[INFO]${nc} Iniciando o processo..."
+    echo -e "${b_purple}[DEBUG]${nc} Variável X = 10"
+}
+
+# Mostra todas as cores disponíveis
+show_colors() {
+    echo -e "\n--- Cores Normais ---"
+    echo -e "${black}Black ${red}Red ${green}Green ${yellow}Yellow ${blue}Blue ${purple}Purple ${cyan}Cyan ${white}White ${nc}"
+    
+    echo -e "\n--- Cores Negrito/Brilhantes ---"
+    echo -e "${b_black}Black ${b_red}Red ${b_green}Green ${b_yellow}Yellow ${b_blue}Blue ${b_purple}Purple ${b_cyan}Cyan ${b_white}White ${nc}"
+
+    echo -e "\nTeste: \033[1;91m old ${nc}"
+
+    echo -e "\n--- Estilos ---"
+    echo -e "${underline}Sublinhado${nc}  ${reverse}Invertido${nc} ${blink}Blink${nc} \n"
+}
